7baf0eb7a858285fc0a16809806639e222833ea0
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2022-06-11 21:28:25 +0000

    some change

--- lib/assistants/assistant_methods.dart
+++ lib/assistants/assistant_methods.dart
@@ -20,7 +20,7 @@ class AssistantMethods
       {
         userModelCurrentInfo = UserModel.fromSnapshot(snap.snapshot);
         print("name ="+userModelCurrentInfo!.name.toString());
-        print("name ="+userModelCurrentInfo!.email.toString());
+        print("email ="+userModelCurrentInfo!.email.toString());
       }
     });
   }
