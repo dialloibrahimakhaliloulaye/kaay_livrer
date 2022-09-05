cd3002f236a9296d08f7154530e8113d0aedd323
aliouneniome <aliouneniome21@gmail.com>
2022-09-05 11:59:10 +0000

    Last modification

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -128,8 +128,8 @@ class _SignUpScreenState extends State<SignUpScreen>
                   color: Colors.grey
                 ),
                 decoration: const InputDecoration(
-                  labelText: "Nom",
-                  hintText: "Nom",
+                  labelText: "Nom complet",
+                  hintText: "Nom complet",
                   prefixIcon: Icon(Icons.account_circle_sharp, color: Colors.white,),
                   enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.grey),
